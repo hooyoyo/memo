fa02ebabf4e3df4da217efbc7fbfac93157c499c
Miro Hone <ot_x41g@yeah.net>
2018-06-15 00:11:23 +0800

    update file

--- js/base.js
+++ js/base.js
@@ -107,10 +107,10 @@ Date.prototype.pattern = function(fmt)
         '6' : '/u516d'
     };
     if(/(y+)/.test(fmt)){
-        fmt=fmt.replace(RegExp.$1, (this.getFullYear()+'').substr(4 - RegExp.$1.length));
+        fmt = fmt.replace(RegExp.$1, (this.getFullYear()+'').substr(4 - RegExp.$1.length));
     }
     if(/(E+)/.test(fmt)){
-        fmt=fmt.replace(RegExp.$1, ((RegExp.$1.length>1) ? (RegExp.$1.length>2 ? '/u661f/u671f' : '/u5468') : '')+week[this.getDay()+'']);
+        fmt = fmt.replace(RegExp.$1, ((RegExp.$1.length>1) ? (RegExp.$1.length>2 ? '/u661f/u671f' : '/u5468') : '')+week[this.getDay()+'']);
     }
     for(var k in o){
         if(new RegExp('('+ k +')').test(fmt)){
