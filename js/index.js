ede0376bb9c5e326f4decad4667270f1f0a4726d
hooyoyo <ot_x41g@yeah.net>
2018-07-11 16:08:08 +0800

    update file

--- js/index.js
+++ js/index.js
@@ -5,16 +5,4 @@ $(function() {
     }*/
     _dropoffUrlParam_();
     $('body').show();
-
-    /*var app_port_i = $('.app-port i');
-    app_port_i.click(function () {
-        var url = $(this).attr('url');
-        window.location.href = url;
-    });*/
-
-    /*var svc_zone_img = $('.svc-zone img');
-    svc_zone_img.click(function () {
-        var url = $(this).attr('url');
-        window.location.href = url;
-    });*/
 });
